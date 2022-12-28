949d4439a733acb7ba15de2d752530498f89b608
BongSuCHOI <76158466+BongSuCHOI@users.noreply.github.com>
2022-12-28 13:42:45 +0900

    기본 폰트 추가 및 로고 이미지로 변경

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { getCategory } from 'prisma/db';
 
 import Category from '@components/Category';
@@ -18,13 +20,15 @@ export default async function RootLayout({ children }) {
 				<div className="max-w-screen-lg px-4 mx-auto relative">
 					<TradingviewTickerWidget />
 					<div className="my-20 text-center">
-						<h1 className="text-8xl font-bold">STOCK</h1>
+						<h1 className="text-8xl font-bold">
+							<Image className="mx-auto" src="/images/logo.svg" width={300} height={90} alt="STOCK" />
+						</h1>
 						<h2 className="text-base mt-6 font-normal">
 							테마별 상위 시가총액 종목과
 							<br />
 							MA(이동평균선) 골드크로스/데드크로스 종목을 알려드립니다.
 							<br />
-							<p className='text-xs leading-6 text-slate-500'>(모든 종목은 리페인팅 방지를 위해 당일 장 마감 후 계산되어 반영됩니다.)</p>
+							<p className="text-xs leading-6 text-slate-500">(모든 종목은 리페인팅 방지를 위해 당일 장 마감 후 계산되어 반영됩니다.)</p>
 						</h2>
 					</div>
 					<TradingviewChartWidget />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
 	content: ['./app/**/*.{js,ts,jsx,tsx}', './page/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
+		fontFamily: {
+			sans: ['Noto Sans KR', ...defaultTheme.fontFamily.sans],
+		},
 		extend: {
 			height: {
 				'72px': '72px',
