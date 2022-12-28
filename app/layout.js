2e4b99a0156bc08350d9adcd6bdaa5139e01b452
BongSuCHOI <76158466+BongSuCHOI@users.noreply.github.com>
2022-12-28 15:58:23 +0900

    logo, favicon, meta data 수정

--- app/layout.js
+++ app/layout.js
@@ -14,17 +14,33 @@ export default async function RootLayout({ children }) {
 	return (
 		<html lang="ko">
 			<head>
-				<title>주식</title>
+				<title>개미집 - 테마별 상위 시가총액, 거래량, 거래량 증가, MA 골드크로스/데드크로스 종목</title>
+				<meta charSet="utf-8"></meta>
+				<meta name="title" content="개미집" />
+				<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta
+					name="keywords"
+					content="국내주식,골드크로스,데드크로스,국장,국내주식테마,주식테마,테마종목,테마정보,주식정보,테마주,코스피,코스닥,급등,급등주,주식,시가총액순위,시총순위,거래량순위,거래량증가"
+				/>
+				<meta name="description" content="테마별 상위 시가총액, 거래량 종목과 거래량 증가 종목, MA(이동평균선) 골드크로스/데드크로스 종목을 알려드립니다." />
+				<meta property="og:type" content="Website" />
+				<meta property="og:title" content="개미집" />
+				<meta property="og:url" content="@@@@@url 필요" />
+				<meta property="og:description" content="테마별 상위 시가총액, 거래량 종목과 거래량 증가 종목, MA(이동평균선) 골드크로스/데드크로스 종목을 알려드립니다." />
+				<meta property="og:image" content="@@@@@og 썸네일 이미지 필요" />
+				<link rel="canonical" href="@@@@@url 필요" />
+				<link rel="icon" href="/favicon.ico" type="image/x-icon" />
 			</head>
 			<body>
 				<div className="max-w-screen-lg px-4 mx-auto relative">
 					<TradingviewTickerWidget />
-					<div className="my-20 text-center">
+					<div className="my-24 text-center">
 						<h1 className="text-8xl font-bold">
-							<Image className="mx-auto" src="/images/logo.svg" width={300} height={90} alt="STOCK" />
+							<Image className="mx-auto" src="/images/logo.svg" width={360} height={90} alt="STOCK" />
 						</h1>
 						<h2 className="text-base mt-6 font-normal">
-							테마별 상위 시가총액 종목과
+							테마별 상위 시가총액, 거래량 종목과 거래량 증가 종목,
 							<br />
 							MA(이동평균선) 골드크로스/데드크로스 종목을 알려드립니다.
 							<br />
