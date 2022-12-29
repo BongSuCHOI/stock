1699c24e655e45670f65d71e5ab17ca67b1d050e
BongSuCHOI <76158466+BongSuCHOI@users.noreply.github.com>
2022-12-29 18:16:54 +0900

    api 파일 수정

--- pages/api/dbUpdateCheck.js
+++ pages/api/dbUpdateCheck.js
@@ -1,26 +1,25 @@
 import { getThemeData } from '@util/db';
 
 export default async function handler(req, res) {
-	const { APP_KEY } = process.env;
-	const { ACCESS_KEY } = req.headers.authorization.split(' ')[1];
-    const allStockData = await getThemeData('all');
+	if (req.method === 'POST') {
+		const allStockData = await getThemeData('all');
+		console.log(allStockData);
+		try {
+			const { authorization } = req.headers;
 
-    console.log(allStockData);
-
-	try {
-		if (ACCESS_KEY === APP_KEY) {
-			console.log('인증 성공');
-
-			// Process the POST request
-			res.status(200).json({ success: 'true' });
-		} else {
-			console.log('인증 실패');
-
-			res.status(401);
+			if (authorization === `Bearer ${process.env.APP_KEY}`) {
+				console.log('인증 성공');
+				res.status(200).json({ success: true });
+			} else {
+				console.log('인증 실패');
+				res.status(401).json({ success: false });
+			}
+		} catch (err) {
+			console.log('catch');
+			res.status(500).json({ statusCode: 500, message: err.message });
 		}
-	} catch (err) {
-		console.log('catch');
-
-		res.status(500);
+	} else {
+		res.setHeader('Allow', 'POST');
+		res.status(405).end('Method Not Allowed');
 	}
 }
